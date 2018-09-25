31a54f812d63e5964b761a65361312edeffe7a11
Marcel Vermeulen <vermeulen.mc@gmail.com>
2018-09-25 09:37:48 +0200

    Update AuthenticationDialog.h

--- src/ios/AuthenticationDialog.h
+++ src/ios/AuthenticationDialog.h
@@ -6,29 +6,25 @@
 #import <Foundation/Foundation.h>
 #import <Cordova/CDVPlugin.h>
 
-@interface AuthenticationDialog: CDVPlugin <NSURLConnectionDataDelegate>
+@interface AuthenticationDialog: CDVPlugin <NSURLSessionDelegate>
 
 @property NSString *uri;
 @property NSString *userName;
 @property NSString *password;
 @property Boolean allowBypassAuth;
 
+
 @property NSString *callbackId;
+
 
 - (void)authenticate:(CDVInvokedUrlCommand*)command;
 - (void)clearCredentials:(CDVInvokedUrlCommand*)command;
 
 @end
 
-
-typedef void (^CredentialsViewCallback)(NSString* userName, NSString* password, BOOL isCancelled);
-
-@interface CredentialsViewController : UIViewController<UIAlertViewDelegate>
-
-@property (copy) CredentialsViewCallback onResult;
-
-- (void)requestUserCredentials:(NSString*)uri withUsername:(NSString*)userName;
-
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex;
-
-@end
+NSURLSessionTask * _task;
+NSString * _host;
+NSURLCredentialPersistence _wantedPersistence;
+NSURLCredential * _storedCredentials;
+BOOL _explicitPersistCredentials;
+BOOL _allowUsernameChange;
